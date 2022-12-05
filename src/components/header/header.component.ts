2f901bd9495c86a1f6986b11ec161d5f0b26553b
Judith <judyfdez97@gmail.com>
2022-12-05 13:18:05 +0100

    Routes fixed

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -12,13 +12,13 @@ class HeaderComponent extends LitElement {
     <header id="header" class="header">
       <nav class="header-navbar">
         <div class="logo-container">
-          <a href="/"><img src="/img/Logo.png" class="logo" alt="brand-logo"></a>
+          <a href="/"><img src="img/Logo.png" class="logo" alt="brand-logo"></a>
           <a href="/"><p class="brand-name">MKIDEA</p></a>
         </div>
         <nav class="navbar-navigation">
-          <a class="nav-item" href="/components/about-us/index.html">About Us</a>
-          <a class="nav-item" href="/components/news/index.html">News</a>
-          <a class="nav-item" href="/components/contact/index.html">Contact</a>
+          <a class="nav-item" href="components/about-us/index.html">About Us</a>
+          <a class="nav-item" href="components/news/index.html">News</a>
+          <a class="nav-item" href="components/contact/index.html">Contact</a>
         </nav>
         <div class="nav-log-container">
           <login-component></login-component>
